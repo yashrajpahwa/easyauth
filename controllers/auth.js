14ecced0709bcfab241897d0262001575e6b39d3
Yashraj Pahwa <59451283+yashrajpahwa@users.noreply.github.com>
2021-07-26 02:41:43 +0530

    Implemented md5 hashing and password hasing in register user route

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,7 +2,9 @@ const { validationResult } = require('express-validator');
 const asyncHandler = require('../middlewares/async');
 const ErrorResponse = require('../utils/errorResponse');
 const mongoUtil = require('../utils/mongoUtil');
+const argon2 = require('argon2');
 const SuccessResponse = require('../utils/successResponse');
+const md5 = require('md5');
 
 // @desc Register new user
 // @route POST /api/v1/auth/register
@@ -12,22 +14,26 @@ exports.registerUser = asyncHandler(async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     const errorComb = Object.values(errors)[1].map((err) => err.msg);
-    res.json(new ErrorResponse(errorComb.join(', '), 400));
-  } else {
-    const hashedPassword = password; // Setup using bcrypt
-    const emailHash = email; // Setup md5 hash
-    const newUser = {
-      name,
-      username,
-      picture: `https://www.gravatar.com/${emailHash}`,
-      email,
-      email_verified: false,
-      given_name,
-      family_name,
-      password: hashedPassword,
-      isDeveloper: false,
-    };
-    // const collection = mongoUtil.getDB().collection('users');
-    res.send(new SuccessResponse('new user route'));
+    return res.status(400).json(new ErrorResponse(errorComb.join(', '), 400));
   }
+  let hashedPassword;
+  try {
+    hashedPassword = await argon2.hash(password);
+  } catch (error) {
+    return res.status(500).json(new ErrorResponse('Server Error', 500));
+  }
+  const emailHash = md5(email); // md5 hash email
+  const newUser = {
+    name,
+    username,
+    picture: `https://secure.gravatar.com/avatar/${emailHash}`,
+    email,
+    email_verified: false,
+    given_name,
+    family_name,
+    password: hashedPassword,
+    isDeveloper: false,
+  };
+  // const collection = mongoUtil.getDB().collection('users');
+  res.status(201).send(new SuccessResponse('new user route', 201, newUser));
 });
