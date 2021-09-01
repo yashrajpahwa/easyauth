1d88dcea21547e680c8658805f7a7147a5d5fb77
Yashraj Pahwa <59451283+yashrajpahwa@users.noreply.github.com>
2021-09-01 12:06:59 +0530

    Added register client route

--- controllers/client/registerClient.js
+++ controllers/client/registerClient.js
@@ -1,13 +1,27 @@
-const { ObjectId } = require('bson');
-const { nanoid } = require('nanoid');
+const { validationResult } = require('express-validator');
 const asyncHandler = require('../../middlewares/async');
+const ErrorResponse = require('../../utils/errorResponse');
+const mongoUtil = require('../../utils/mongoUtil');
+const SuccessResponse = require('../../utils/successResponse');
 
-//@ts-check
 const registerClient = asyncHandler(async (req, res, next) => {
-  const { client_name, contacts } = req.body;
-  const client_secret = nanoid();
+  const { accessTokenPayload } = req;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const errorComb = Object.values(errors)[1].map((err) => err.msg);
+    return res.status(400).json(new ErrorResponse(errorComb.join(', '), res));
+  }
+  const { name, enabled } = req.body;
   const newClient = {
-    client_name,
-    client_secret,
+    name,
+    owner: accessTokenPayload._id,
+    enabled,
   };
+  const collection = mongoUtil.getDB().collection('clients');
+  await collection.insertOne(newClient);
+  res
+    .status(201)
+    .json(new SuccessResponse(res, 'Client has been registered', newClient));
 });
+
+module.exports = registerClient;
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router({ mergeParams: true });
 
 const auth = require('./auth');
+const client = require('./client');
 
 router.use(`/auth`, auth);
+router.use(`/client`, client);
 
 module.exports = router;
